1755f29a3ce50a7f2f009d006ecaeaa8ba708548
ttoloza <tomasezequieltoloza@gmail.com>
2022-06-15 00:11:13 -0300

    Update connections

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,45 @@ import {MysqlConnectionOptions} from "typeorm/driver/mysql/MysqlConnectionOption
 
 const dotenv = require('dotenv').config();
 
-
 const dataSourceOptionsTaller: MysqlConnectionOptions = {
-  type: 'mariadb',
-  database:  process.env.DB_NAME,
-  host:  process.env.DB_HOST,
+  type: 'mysql',
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT as unknown as number,
+  port: parseInt(process.env.DB_PORT),
 }
 
-export const dataSourceTaller = new DataSource(dataSourceOptionsTaller);
+const dataSourceDockerMySQL: MysqlConnectionOptions = {
+  type: 'mysql',
+  database: "disenio",
+  host: "localhost",
+  username: "root",
+  password: "disenio",
+  port: 3306,
+  logging: true,
+  synchronize: true,
+  entities: [
+    "entity/*.js"
+  ],
+}
+
+const dataSourceOptionsHeroku: MysqlConnectionOptions = {
+  type: 'mariadb',
+  url: process.env.HEROKU_DB_URL
+}
+
+export const dataSourceHeroku = new DataSource(dataSourceOptionsHeroku);
+export const dataSourceTaller = new DataSource(dataSourceDockerMySQL);
+export const dataSourceDocker = new DataSource(dataSourceDockerMySQL);
+
+
+export const createConnection = async (connectionDataSource: DataSource) => {
+  await connectionDataSource.initialize()
+    .then(() => {
+      console.log("Data source initialized. Database used => ", connectionDataSource.options.database)
+    })
+    .catch((err) => {
+      console.error("Cannot initialize data source => ", err)
+    })
+};
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import {dataSourceTaller} from "./data-source"
-import {User} from "./entity/User"
+import {createConnection, dataSourceHeroku} from "./data-source"
 
-const validateConnection = () => {
-  dataSourceTaller.initialize()
-    .then(() => {
-      dataSourceTaller.query("SELECT * FROM estudiante where 'estudiante_id'=1").then(r => {
-        console.log(r)
-      })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
-
-validateConnection()
+createConnection(dataSourceHeroku)
