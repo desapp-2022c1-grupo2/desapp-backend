5bbe91eb39d03becf603ad0199019cc016d6adb3
adrian <agyaniri@gmail.com>
2022-06-17 19:09:38 -0300

    conexion a heroku configurada

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,27 @@ import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { AssignmentModule } from './assignment/assignment.module';
 import {ConfigModule} from "@nestjs/config";
+import { DatabaseModule } from './database/database.module';
+import * as Joi from '@hapi/joi';
+
+
 
 @Module({
   imports: [
-      ConfigModule.forRoot(),
+      ConfigModule.forRoot({
+          validationSchema: Joi.object({        // valida el formato de las credenciales
+              //HEROKU_URL: Joi.string().required(),
+              HEROKU_HOST: Joi.string().required(),
+              HEROKU_PORT: Joi.number().required(),
+              HEROKU_USER: Joi.string().required(),
+              HEROKU_PASSWORD: Joi.string().required(),
+              HEROKU_DB: Joi.string().required(),
+              PORT: Joi.number(),
+          })
+      }),
       StudentModule,
-      AssignmentModule
+      AssignmentModule,
+      DatabaseModule
   ] ,
   controllers: [AppController],
   providers: [AppService],
