3590c5e5029d2e176fcd64b959d1e308972fae2d
Tomas Toloza <tomasezequieltoloza@gmail.com>
2022-07-18 00:39:35 -0300

    Change student endpoint

--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -4,7 +4,7 @@ import {BaseController} from "../../commons";
 import {Student} from "./entities";
 import { BaseService } from 'src/commons/service.commons';
 
-@Controller('students')
+@Controller('student')
 export class StudentController extends BaseController<Student> {
 
   constructor( private readonly studentService: StudentService ) {
