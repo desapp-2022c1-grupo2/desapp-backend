cbe65dbd84d7699a45c22fc4a6cd8c3ba9c91fb3
adrian <agyaniri@gmail.com>
2022-07-19 16:02:40 -0300

    [refarctor] se agregan campos a la entidad assignment.entity

--- src/modules/assignment/entities/assignment.entity.ts
+++ src/modules/assignment/entities/assignment.entity.ts
@@ -5,22 +5,64 @@ import {BaseEntity} from "../../../commons";
 @Entity('trabajo')
 export class AssignmentEntity extends BaseEntity {
 
-    @PrimaryGeneratedColumn('increment',{name:'trabajo_id'})
+    @PrimaryGeneratedColumn('increment',{name:'trabajo_id',})
     id: number;
 
-    @Column({ name: 'jtp_id'})
+    @Column({ name: 'jtp_id', nullable: true })
     jtp_id: number;
 
     @Column({name: 'numero'})
     number?: number;
 
-    @Column({ name: 'nombre'})
+    @Column({ name: 'nombre', nullable: true})
     name: string;
 
     @Column({ name: 'url'})
     url: string;
 
-    @Column( { name: 'descripcion'})
+    @Column( {name: 'descripcion_corta', nullable: true})
+    descripcion_corta: string;
+
+    @Column( { name: 'descripcion', nullable: true})
     description: string;
+
+    @Column({ name: 'consigna',nullable: true})
+    consigna: string
+
+    @Column({ name: 'fecha_inicio',nullable: true})
+    fechaInicio: Date;
+
+    @Column({ name: 'fecha_entrega', nullable: true})
+    fechaEntrega: Date;
+
+    @Column({ name: 'etiquetas', nullable: true})
+    etiquetas: string
+
+    @Column({ name: 'variable1', nullable: true})
+    variable1: string;
+
+    @Column({ name: 'variable2', nullable: true})
+    variable2: string;
+
+    @Column({ name: 'variable3', nullable: true})
+    variable3: string;
+
+    @Column({ name: 'variable4', nullable: true})
+    variable4: string;
+
+    @Column({ name: 'variable5', nullable: true})
+    variable5: string;
+
+    @Column({ name: 'tipo', nullable: true} )
+    tipo: number;
+
+    @Column({ name: 'estado', nullable: true})
+    estado: number;
+
+    @Column({ name: 'materia_id',nullable: true})
+    materiaId: number;
+
+    @Column({ name: 'proceso_individual', nullable: true})
+    procesoIndividual: number;
 
 }
