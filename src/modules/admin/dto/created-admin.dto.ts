fa23290a601afeb610f7b769e71d0483acfe65cd
Adrian <agyaniri@gmail.com>
2022-11-23 22:57:09 -0300

    validaciones en admins

--- src/modules/admin/dto/created-admin.dto.ts
+++ src/modules/admin/dto/created-admin.dto.ts
@@ -1,9 +1,36 @@
-import { IsString } from 'class-validator';
+import {
+  IsDefined,
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreatedAdminDto {
+  @IsDefined()
+  @MinLength(3)
+  @MaxLength(20)
   @IsString()
   readonly name: string;
 
+  @IsDefined()
+  @MinLength(3)
+  @MaxLength(20)
   @IsString()
   readonly lastName: string;
+
+  @IsDefined()
+  @MinLength(3)
+  @MaxLength(20)
+  @IsEmail()
+  readonly email: string;
+
+  @IsDefined()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message: 'Password too weak',
+  })
+  password: string;
 }
