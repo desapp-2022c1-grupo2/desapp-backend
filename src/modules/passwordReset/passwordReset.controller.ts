6e14e8497caf3a42b01974ee03359ea3b4f23277
Tomás Toloza <tomasezequieltoloza@gmail.com>
2022-11-30 20:28:28 -0300

    Update passwordReset.controller.ts

--- src/modules/passwordReset/passwordReset.controller.ts
+++ src/modules/passwordReset/passwordReset.controller.ts
@@ -6,13 +6,6 @@ export class PasswordResetController {
     constructor(private readonly passwordResetService: PasswordResetService) {
     }
 
-    // @Public()
-    // @Post('validate/:id')
-    // async validateResetById(@Param('id') resetId: any, @Body() body: any) {
-    //     const password = body.password;
-    //     return this.passwordResetService.isValid(resetId, password);
-    // }
-
     @Post('')
     async resetPasswordById(@Body() body: { mail: string, role: "JTP" | "ADMIN", id: string }) {
         const {mail, role, id} = body
